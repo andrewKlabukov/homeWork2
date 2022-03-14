c446e7300bd30553400feb81ff5f5ed8fc1f427e
Andrew Klabukov <Andrew.klabukov@yandex.ru>
2022-03-14 09:30:28 +0400

    change js file

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Задание 1
+//  Задание 1
 
 function makeObjectDeepCopy(obj) {
   const copyObj = Array.isArray(obj) ? [] : {};
@@ -17,7 +17,7 @@ function makeObjectDeepCopy(obj) {
   return copyObj;
 }
 
-// Задание 2
+//  Задание 2
 
 function selectFromInterval(array, firstValue, secondValue) {
   const isValid = Array.isArray(array) && array.every( (item) => typeof item === 'number' );
@@ -25,12 +25,11 @@ function selectFromInterval(array, firstValue, secondValue) {
 
   if (!isValid) {
       throw new Error('Ошибка!');
-  };
-  
+  };  
   return result;
 }
 
-// Задание 3
+//  Задание 3
 
 const myIterable = {
   from: 1,
